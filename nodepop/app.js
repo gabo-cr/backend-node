b4144aa82600d77681ab86ee50249d8834d959e7
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-02-22 19:18:23 -0600

    Manejo mejor los errores. Devuelvo un json si ocurrió un error en el api.

--- nodepop/app.js
+++ nodepop/app.js
@@ -8,9 +8,6 @@ var nunjucks = require('nunjucks');
 
 require('./lib/connectMongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -20,7 +17,9 @@ nunjucks.configure('views', {
 });
 app.set('view engine', 'njk')
 
-// MIDDLEWARES
+/** 
+ * Middlewares
+ */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,8 +27,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/** 
+ * Rutas del API
+ */
+app.use('/api/anuncios', require('./routes/api/anuncios'));
+
+/** 
+ * Rutas del Website
+ */
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,12 +45,27 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  // errores de validación
+  if (err.array) {
+    const errInfo = err.array({ })[0];
+    console.log(errInfo);
+    err.message = `Not valid - ${errInfo.type} ${errInfo.path} in ${errInfo.location} ${errInfo.msg}`;
+    err.status = 422;
+  }
+
+  res.status(err.status || 500);
+
+  // si el fallo es en el API, responder en formato JSON
+  if (req.originalUrl.startsWith('/api/')) {
+    res.json({ error: err.message });
+    return;
+  }
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
   res.render('error');
 });
 
