d451822fb06e4e4d129ba733018a500067ce638e
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-02-23 15:33:15 -0600

    Agrego el script lint:fix a package.json y corrijo el resto de errores y warnings.

--- nodepop/models/Anuncio.js
+++ nodepop/models/Anuncio.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 // Schema de un anuncio
 const anuncioSchema = mongoose.Schema({
-	nombre: { type: String, required: true, unique:true },
-	venta: { type: Boolean },
-	precio: { type: Number, required: true, min: 0.0 },
-	foto: { type: String },
-	tags: [String]
+  nombre: { type: String, required: true, unique: true },
+  venta: { type: Boolean },
+  precio: { type: Number, required: true, min: 0.0 },
+  foto: { type: String },
+  tags: [String]
 });
 
 const Anuncio = mongoose.model('Anuncio', anuncioSchema);
