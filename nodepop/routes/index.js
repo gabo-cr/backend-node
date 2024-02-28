b0187f3fa9cb7599aba7563cc0177e7907418be7
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-02-27 21:10:18 -0600

    Agrego la página de inicio con todos los anuncios y un estilo básico inicial.

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Anuncio = require('../models/Anuncio');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  const filter = {};
+  const start = 0;
+  const limit = 0;
+  const sort = 'nombre';
+
+  const anuncios = await Anuncio.listarAnuncios(filter, start, limit, sort);
+
+  const context = {
+    title: 'Nodepop',
+    anuncios
+  };
+  res.render('index', context);
 });
 
 module.exports = router;
