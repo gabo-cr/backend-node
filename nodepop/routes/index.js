222e860c3b516edd7d5b6b103ba2797a40247ef4
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-02-28 16:00:49 -0600

    Agrego la funcionalidad completa de los filtros.

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -4,18 +4,52 @@ const Anuncio = require('../models/Anuncio');
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
+  const filterByNombre = req.query.nombre;
+  const filterByVenta = req.query.venta;
+  const filterByPreciomin = req.query.preciomin;
+  const filterByPreciomax = req.query.preciomax;
+  const filterByTags = req.query.tags;
+
   const filter = {};
   const start = 0;
   const limit = 0;
   const sort = 'nombre';
 
+  if (filterByNombre) {
+    filter.nombre = new RegExp('^' + filterByNombre, 'i');
+  }
+  if (filterByVenta) {
+    filter.venta = filterByVenta === 'venta';
+  }
+  if (filterByTags) {
+    filter.tags = { $in: filterByTags };
+  }
+  if (filterByPreciomin && filterByPreciomax) {
+    filter.precio = { $lte: filterByPreciomax, $gte: filterByPreciomin };
+  } else if (filterByPreciomin && !filterByPreciomax) {
+    filter.precio = { $gte: filterByPreciomin };
+  } else if (!filterByPreciomin && filterByPreciomax) {
+    filter.precio = { $lte: filterByPreciomax };
+  }
+
   const anuncios = await Anuncio.listarAnuncios(filter, start, limit, sort);
   const tags = await Anuncio.listarTags();
+  const tipos = [
+    { value: '', label: 'Seleccionar el tipo' },
+    { value: 'venta', label: 'Se vende' },
+    { value: 'busca', label: 'Se busca' }
+  ];
 
   const context = {
     title: 'Nodepop',
     anuncios,
-    tags
+    tags,
+    tipos,
+    filterByNombre,
+    filterByPreciomin,
+    filterByPreciomax,
+    filterByTags: filterByTags || [],
+    filterByVenta
   };
   res.render('index', context);
 });
