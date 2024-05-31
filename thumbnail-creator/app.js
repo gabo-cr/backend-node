248e599566f313e7b1cfeb4e55d9eb5e3ae7ba78
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-05-30 18:36:04 -0600

    Add thumbnail creator node app

---
+++ thumbnail-creator/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const path = require('node:path');
+const jimp = require('jimp');
+const { Responder } = require('cote');
+
+const responder = new Responder({ name: 'thumbnail creator microservice' });
+
+responder.on('create-thumbnail', (req, done) => {
+	const { filename } = req;
+
+	console.log(Date.now(), 'servicio:', filename);
+	
+	const route = path.join(__dirname, '..', 'nodepop','public', 'images', 'anuncios');
+	
+	const dotIndex = filename.lastIndexOf('.');
+	const newRoute = path.join(__dirname, '..', 'nodepop','public', 'images', 'anuncios', 'thumbnails');
+	const newFilename = `${filename.slice(0, dotIndex)}-thumbnail.${filename.slice(dotIndex+1)}`;
+
+	jimp.read(`${route}/${filename}`, (err, image) => {
+		if (err) throw err;
+		image
+			.resize(100, 100)
+			.write(`${newRoute}/${newFilename}`);
+	});
+
+	done(`Guardada en ${newRoute}/${newFilename}`);
+});
